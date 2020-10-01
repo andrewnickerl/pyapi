090338bae0c04185d2ded2726a4ae40603ad41fe
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-10-01 08:37:43 -0700

    Adds pokemon api challenge

---
+++ pokeapi_challenge/challenge.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import requests
+import wget
+
+def api_pull():
+    choice = input("What Pokemon would you like a picture of? ").lower()
+    pokedict = requests.get(f"https://pokeapi.co/api/v2/pokemon/{choice}").json()
+    return pokedict
+
+def api_slice(json2python):
+    poke_pic = json2python['sprites']['front_default']
+    return poke_pic
+
+def wget_pic(imagelink):
+    wget.download(imagelink, '/home/student/mycode')
+
+def main():
+    wget_pic(api_slice(api_pull()))
+
+if __name__ == '__main__':
+    main()
